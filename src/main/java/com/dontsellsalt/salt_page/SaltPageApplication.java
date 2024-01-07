fa148b9708baed1a1685f0e0253f73cf6b8ccc07
E-Been <been@gentleenergycorp.com>
2024-01-07 22:50:18 +0900

    git init

---
+++ src/main/java/com/dontsellsalt/salt_page/SaltPageApplication.java
@@ -0,0 +1,13 @@
+package com.dontsellsalt.salt_page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaltPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaltPageApplication.class, args);
+	}
+
+}
